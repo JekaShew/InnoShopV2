ed6b6cfadeee30afdb13d3502e2d329ae28c1d7e
JekaShew <djouker1546@gmail.com>
2024-12-19 23:26:13 +0300

    CommonLibrary

---
+++ InnoShop.CommonLibrary/InnoShop.CommonLibrary/DependencyInjection/CommonServiceContainer.cs
@@ -0,0 +1,50 @@
+﻿using InnoShop.CommonLibrary.Middleware;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InnoShop.CommonLibrary.DependencyInjection
+{
+    public static class CommonServiceContainer
+    {
+        public static IServiceCollection AddCommonServices<TContext>
+            (this IServiceCollection services, IConfiguration configuration, string fileName) where TContext : DbContext
+        {
+            // DB 
+            services.AddDbContext<TContext>(option => option.UseSqlServer(
+                configuration.GetConnectionString("InnoShop"), sqlserverOption => sqlserverOption.EnableRetryOnFailure()));
+
+            // Serilog logger
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel
+                .Information()
+                .WriteTo.Debug()
+                .WriteTo.Console()
+                .WriteTo.File(path: $"{fileName}-.log",
+                restrictedToMinimumLevel: Serilog.Events.LogEventLevel.Information,
+                outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {message:lj}{NewLine}{Exception}",
+                rollingInterval: RollingInterval.Day)
+                .CreateLogger();
+
+            // JWT Authentication Scheme
+            JWTAuthenticationScheme.AddJWTAuthenticationScheme(services, configuration);
+
+            return services;
+        }
+        public static IApplicationBuilder UseCommonPolicies(this IApplicationBuilder app)
+        {
+            app.UseMiddleware<GlobalException>();
+
+            app.UseMiddleware<ListenToOnlyApiGatewayRule>();
+
+            return app;
+        }
+    }
+}
---
+++ InnoShop.CommonLibrary/InnoShop.CommonLibrary/Middleware/ListenToOnlyApiGatewayRule.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InnoShop.CommonLibrary.Middleware
+{
+    public class ListenToOnlyApiGatewayRule(RequestDelegate next)
+    {
+        public async Task InvokeAsync(HttpContext context)
+        {
+            var signedHeader = context.Request.Headers["ApiGateway"];
+
+            if (signedHeader.FirstOrDefault() is null)
+            {
+                context.Response.StatusCode = StatusCodes.Status503ServiceUnavailable;
+                await context.Response.WriteAsync("Sorry! Service is unavailable!");
+                return;
+            }
+            else
+            {
+                await next(context);
+            }
+
+        }
+    }
+}
